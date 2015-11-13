c6901031e91bae6aeb95f9dbfd6eb103856a5a0d
LucasFreitas <lucas_but@hotmail.com>
2015-11-13 00:44:30 -0200

    Initial Commit

---
+++ Nikin_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nikin_App
+//
+//  Created by Lucas Freitas de Oliveira on 11/13/15.
+//  Copyright © 2015 Lucas Freitas de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
